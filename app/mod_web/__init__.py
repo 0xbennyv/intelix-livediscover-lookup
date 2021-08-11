5023851cab62b8a717535b91376c4907e0889685
Ben <ben@bennyv.com>
2021-08-11 17:28:04 +1000

    Added a landing page

--- app/mod_web/__init__.py
+++ app/mod_web/__init__.py
@@ -5,14 +5,14 @@ from flask import Blueprint, request, redirect, url_for, render_template
 mod_web = Blueprint('web', __name__, url_prefix='/')
 @mod_web.route("/")
 def index():
-    return f"<p> Setup Sophos Live discover to point to {request.base_url}lookup/ioc </p>"
+    return render_template('mod_web/index.html', response=request.base_url)
 
 @mod_web.route("lookup/")
 def lookup():
     return redirect(url_for('web.index'))
 
 @mod_web.route("lookup/<ioc>")
-def ioc(ioc):
+def lookupioc(ioc):
     if validators.ipv4(ioc) or validators.md5(ioc) or validators.sha256(ioc):
         r = intelixlookup(ioc)
         return r
@@ -23,7 +23,7 @@ def ioc(ioc):
 def ui(ioc):
     if validators.ipv4(ioc) or validators.md5(ioc) or validators.sha256(ioc):
         r = intelixlookup(ioc)
-        return render_template('mod_web/index.html', response=r)
+        return render_template('mod_web/lookup.html', response=r)
     else:
         return redirect(url_for('web.index'))
     
